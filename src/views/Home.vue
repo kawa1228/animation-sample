f787a6001db0893d58f50194d25b96cf086abf10
kawa1228 <sxa.1105bo@gmail.com>
2020-01-12 15:28:38 +0900

    gsapを追加

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
+    <img ref="vue-logo" alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
@@ -8,11 +8,17 @@
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import {TweenMax} from "gsap";
 
 export default {
   name: 'home',
   components: {
     HelloWorld
+  },
+  mounted() {
+    const logo = this.$refs['vue-logo']
+    console.log('logo', logo)
+    TweenMax.to(logo, 1, {opacity: 0});
   }
 }
 </script>
