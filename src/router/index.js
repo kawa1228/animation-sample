431464f18b6aa08aea2a3f5375c37572f8af8853
kawa1228 <sxa.1105bo@gmail.com>
2020-01-12 21:13:57 +0900

    タイポを修正

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
-import Animation from '../views/Animation'
+import Animation from '../views/Animation.vue'
 
 Vue.use(VueRouter)
 
