b03e9ea164f20557797ffb3078ad13422df03efa
kawa1228 <sxa.1105bo@gmail.com>
2020-01-12 15:28:13 +0900

    animation componentを新規作成

---
+++ src/views/Animation.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="animation">
+    <div class="content">
+      <h1>Dolphin is cute</h1>
+      <img src="../assets/image01.jpeg" alt="dolphin">
+    </div>
+    <div class="content">
+      <h1>Stingray is beautiful</h1>
+      <img src="../assets/image02.jpeg" alt="stingray">
+    </div>
+    <div class="content">
+      <h1>Penguin looks cool</h1>
+      <img src="../assets/image03.jpeg" alt="penguin">
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import {TweenMax} from "gsap";
+
+export default {
+  name: 'animation',
+  components: {
+    //
+  },
+  mounted() {
+    //
+  }
+}
+</script>
